4a36b2c20353f6ae131667b96d3b22a869696cea
diya866 <diyabhambore@gmail.com>
2026-04-02 16:47:53 +0530

    Add files via upload

---
+++ SpringBootSecutity1/src/main/java/com/test/SpringBootSecutity1Application.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecutity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecutity1Application.class, args);
+	}
+
+}

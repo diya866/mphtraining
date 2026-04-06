c090c274d65900d7dd23e8eaaacb1798d9459df6
diya866 <diyabhambore@gmail.com>
2026-04-06 10:20:35 +0530

    Add files via upload

---
+++ SpringBootWebFluxMySql/src/main/java/com/test/controller/CustomerController.java
@@ -0,0 +1,43 @@
+package com.test.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.test.entity.Customer;
+import com.test.repository.CustomerRepository;
+import com.test.service.CustomerService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+	
+	@Autowired(required = true)
+	private CustomerService service;
+	
+//	public CustomerController(CustomerRepository repository)
+//	{
+//		this.repository = repository;
+//	}
+	
+	@PostMapping("/create")
+	public Mono<Customer> createCustomer(@RequestBody Customer cst)
+	{
+		
+		return service.saveCustomer(cst);
+	}
+	
+	@GetMapping("/list")
+	public Flux<Customer> listAllCustomers()
+	{
+		return service.getAllCustomers();
+	}
+}
